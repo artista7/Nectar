30484f1453b59e7b6e56b6654d960e3943780761
Rajat Raj <rajatraj733@gmail.com>
2018-03-02 03:34:44 +0530

    Fixing Bug to map coin for coinome Handling error in save-data.js

--- crypto-exchange-api/mapper/coinome.mapper.js
+++ crypto-exchange-api/mapper/coinome.mapper.js
@@ -1,22 +1,22 @@
 
 
-var mappings = {
-    'btc-inr': 'BTC',
-    'bch-inr': 'BCH',
-    'ltc-inr': 'LTC',
-    'dash-inr': 'DASH',
-    'dgb-inr': 'DGB'
-};
 
 coinomeMapper = function(coinomeRes, coinObj) {
     let coinomeObj = coinObj['coinome'] = {};
     for(const coin in coinomeRes) {
-        var mappedCoin = mappings[coin];
+        var mappedCoin = getCoinName(coin);
         coinomeObj[mappedCoin] = {};
         coinomeObj[mappedCoin].price = coinomeRes[coin]['last'];
         coinomeObj[mappedCoin].volume = coinomeRes[coin]['24hr_volume'];
     }
     return coinObj;
+}
+
+getCoinName = function(mapping) {
+    let i = mapping.indexOf('-');
+    let mappedCoin = mapping.substring(0, i);
+    mappedCoin = mappedCoin.toUpperCase();
+    return mappedCoin;
 }
 
 module.exports = coinomeMapper;
