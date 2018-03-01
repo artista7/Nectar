26552f9d7ff854d03f14e608171172c6707c4fce
Rajat Raj <rajatraj733@gmail.com>
2018-03-02 01:17:06 +0530

    made changes in config file

--- crypto-exchange-api/consts/config.js
+++ crypto-exchange-api/consts/config.js
@@ -3,14 +3,14 @@
 var config = {
     production: {
         database: {
-            url: 'mongodb://mongodb/crypto',
+            url: 'mongodb://webrole:webrolePasswordMongo123!@mongodb/crypto',
             database: 'crypto',
             collection: 'cryptoData'
         }
     },
     development: {
         database: {
-            url: 'mongodb://mongodb/crypto',
+            url: 'mongodb://webrole:passwordMongo123!@mongodb/crypto',
             database: 'crypto',
             collection: 'cryptoData'
         }
