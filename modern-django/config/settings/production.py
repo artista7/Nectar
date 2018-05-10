0b0779591ab93243971415e6ab28b02e826055c9
Artista7 <shubham1.6180@gmail.com>
2018-05-11 02:29:49 +0530

    huh

--- modern-django/config/settings/production.py
+++ modern-django/config/settings/production.py
@@ -2,4 +2,4 @@ from .base import *
 
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='jds&j%4+(*yzz5@@c^3q(t0u!v(64+2y2ma1$92fd)m_#fxz&$')
 
-ALLOWED_HOSTS = ['www.cryptobee.in']
+ALLOWED_HOSTS = ['139.59.73.127']
